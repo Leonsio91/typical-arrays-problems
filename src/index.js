2b7776a1755be6d1726ef12ba2780965f5565540
Levon Voskaniangit <suxoy1991@gmail.com>
2021-03-14 21:18:21 +0400

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array == null || array.length === 0) {
+		return 0;
+	}
+	let minValue = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] < minValue) {
+			minValue = array[i];
+		}
+	}
+	return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array == null || array.length === 0) {
+		return 0;
+	}
+	let mixValue = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] > mixValue) {
+			mixValue = array[i];
+		}
+	}
+	return mixValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array == null || array.length === 0) {
+		return 0;
+	}
+	let sumValue = 0;
+	for (let i = 0; i < array.length; i++) {
+		sumValue = sumValue + array[i];
+	}
+	return sumValue / array.length;
 }
